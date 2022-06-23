ea244949f54bf31b7b43874c86008fcb01336f0a
LUIZ FELIPE RADOLL CORDEIRO <luiz.cordeiro2@alunos.sc.senac.br>
2022-06-23 14:09:56 -0300

    arrumei o footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,10 @@
 </body>
 <footer>
-    <p>oi</p>
+    <hr class="linha">
+    <p>Programmer World é um site especialmente para programadores de todos os niveis
+        com o intuito<br>de debatermos entre nós e ajudarmos uns aos outros tirando duvidas ou só conversando mesmo.
+        <br>PW (Programmer World) também pode ser usado para você postar os seus projetos e outros programadores
+        <br>poderão avaliar o seu projeto e dar sugestõens de melhorias.
+    </p>
+    <img src="../img/logo-pw.png">
 </footer>
