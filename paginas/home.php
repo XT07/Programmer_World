7f3159cc44f3517848c6afa3f740bf3c79a86dbe
LUIZ FELIPE RADOLL CORDEIRO <luiz.cordeiro2@alunos.sc.senac.br>
2022-06-24 11:38:35 -0300

    fazendo o drop menu

--- paginas/home.php
+++ paginas/home.php
@@ -1,23 +1,41 @@
 <?php
     require("../templates/header.php");
 ?>
-<body>
+<body style="margin: 0; padding: 0;">
     <input type="checkbox" id="menu" class="checkMenu">
     <label for="menu" class="lMenu">
         <img src="../img/menu.png" class="imgMenu">
     </label>
     <div class="navMenu">
         <ul class="ulMenu">
-            <li><a href='log-out.php' class="aMenu">Amigos</a></li>
-        </ul>
-        <ul class="aMenu">
-            <li><a href='log-out.php' class="aMenu">Chat</a></li>
-        </ul>
-        <ul class="aMenu">
-            <li><a href='log-out.php' class="aMenu">Chat privado</a></li>
-        </ul>
-        <ul class="aMenu">
-            <li><a href='log-out.php' class="aMenu">Encerrar sessão</a></li>
+            <li class="liMenu">
+                <a href='log-out.php' class="aMenu">Amigos</a>
+            </li>
+            <li class="liMenu">
+                <a href='log-out.php' class="aMenu">Chat</a>
+                <ul class="ul-chat">
+                    <li><a href=''>público</a></li>
+                    <li><a href=''>privado</a></li>
+                </ul>
+            </li>
+            <li class="liMenu">
+                <a href='log-out.php' class="aMenu">Projetos</a>
+                <ul class="ul-pro">
+                    <li><a href="">de amigos</a></li>
+                    <li><a href="">público</a></li>
+                    <li><a href="">meus projetos</a></li>
+                </ul>
+            </li>
+            <li class="liMenu">
+                <a href='log-out.php' class="aMenu">Comunidade</a>
+            </li>
+            <li class="liMenu">
+                <a href='log-out.php' class="aMenu">Conta</a>
+                <ul class="ul-conta">
+                    <li><a href="">configurações</a></li>
+                    <a href='log-out.php' class="aMenu">Encerrar sessão</a>
+                </ul>
+            </li>
         </ul>
     </div>
 </body>
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <hr class="linha">
+    <hr>
     <p>Programmer World é um site especialmente para programadores de todos os niveis
         com o intuito<br>de debatermos entre si e ajudarmos uns aos outros tirando duvidas ou só conversando mesmo.
         <br>PW (Programmer World) também pode ser usado para você postar os seus projetos e outros programadores
